597826621dfd14b13a16e8cdc9600ae4e61337aa
dmitrii64 <dmitriizykov64@gmail.com>
2015-11-03 20:03:27 +0300

    Big example

--- test.c
+++ test.c
@@ -1,16 +1,44 @@
 
-int func()
+void func_without_args()
 {
 }
 
-int func2(int b)
+int func_with_one_arg(int arg)
 {
-    int a = 6;
+    int a = arg*2;
+    return a;
 }
 
-int main(int a, int b)
+int func_with_two_args(int arg1,int arg2)
+{
+    int a = arg1*123+arg2*987;
+    return a;
+}
+
+int main()
 {
     int i;
-    // (true) ? (i = 1) : (i = 2) ;
+    int abc;
+    (true) ? (i = 1) : (i = 2) ;
+
+    if(i==1)
+    {
+        abc = 123;
+        abc = abc + 333;
+    }
+    else
+    {
+    }
+
+    while(abc>100)
+    {
+        abc--;
+    }
+
+    for(i=0;i<100;i++)
+    {
+        abc++;
+    }
+
     return 0;
 }
