ee52ebd8a923ff8a91c55d0772ca636021036598
Ivan Pushkin <pushkin13@bk.ru>
2015-11-07 00:00:24 +0300

    code refactoring

--- src/edu/eltech/moevm/parsing_tree/TreeRecursionOptimizer.java
+++ src/edu/eltech/moevm/parsing_tree/TreeRecursionOptimizer.java
@@ -16,135 +16,135 @@ public class TreeRecursionOptimizer implements PTCallback {
             }
             PTNode ptnodeChild = (PTNode)child;
             switch (ptnodeChild.getNonterminal()) {
-            case POSTFIX_EXPRESSION:
-                // childs = ["postfix_expression","INC_OP"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.POSTFIX_EXPRESSION && element1 instanceof PTLeaf && ((PTLeaf)element1).getToken() == Parser.INC_OP) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                case POSTFIX_EXPRESSION:
+                    // childs = ["postfix_expression","INC_OP"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.POSTFIX_EXPRESSION && element1 instanceof PTLeaf && ((PTLeaf)element1).getToken() == Parser.INC_OP) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                // childs = ["postfix_expression","DEC_OP"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.POSTFIX_EXPRESSION && element1 instanceof PTLeaf && ((PTLeaf)element1).getToken() == Parser.DEC_OP) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    // childs = ["postfix_expression","DEC_OP"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.POSTFIX_EXPRESSION && element1 instanceof PTLeaf && ((PTLeaf)element1).getToken() == Parser.DEC_OP) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                break;
-            case UNARY_EXPRESSION:
-                // childs = ["INC_OP","unary_expression"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTLeaf && ((PTLeaf)element0).getToken() == Parser.INC_OP && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.UNARY_EXPRESSION) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    break;
+                case UNARY_EXPRESSION:
+                    // childs = ["INC_OP","unary_expression"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTLeaf && ((PTLeaf)element0).getToken() == Parser.INC_OP && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.UNARY_EXPRESSION) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                // childs = ["DEC_OP","unary_expression"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTLeaf && ((PTLeaf)element0).getToken() == Parser.DEC_OP && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.UNARY_EXPRESSION) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    // childs = ["DEC_OP","unary_expression"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTLeaf && ((PTLeaf)element0).getToken() == Parser.DEC_OP && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.UNARY_EXPRESSION) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                // childs = ["SIZEOF","unary_expression"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTLeaf && ((PTLeaf)element0).getToken() == Parser.SIZEOF && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.UNARY_EXPRESSION) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    // childs = ["SIZEOF","unary_expression"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTLeaf && ((PTLeaf)element0).getToken() == Parser.SIZEOF && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.UNARY_EXPRESSION) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                break;
-            case DECLARATION_SPECIFIERS:
-                // childs = ["type_specifier","declaration_specifiers"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.TYPE_SPECIFIER && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.DECLARATION_SPECIFIERS) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    break;
+                case DECLARATION_SPECIFIERS:
+                    // childs = ["type_specifier","declaration_specifiers"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.TYPE_SPECIFIER && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.DECLARATION_SPECIFIERS) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                break;
-            case DECLARATION_LIST:
-                // childs = ["declaration_list","declaration"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.DECLARATION_LIST && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.DECLARATION) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    break;
+                case DECLARATION_LIST:
+                    // childs = ["declaration_list","declaration"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.DECLARATION_LIST && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.DECLARATION) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                break;
-            case STATEMENT_LIST:
-                // childs = ["statement_list","statement"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.STATEMENT_LIST && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.STATEMENT) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    break;
+                case STATEMENT_LIST:
+                    // childs = ["statement_list","statement"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.STATEMENT_LIST && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.STATEMENT) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                break;
-            case TRANSLATION_UNIT:
-                // childs = ["translation_unit","external_declaration"]
-                if (ptnodeChild.getElements().size() == 2) {
-                    PTElement element0 = ptnodeChild.getElements().get(0);
-                    PTElement element1 = ptnodeChild.getElements().get(1);
-                    if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.TRANSLATION_UNIT && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.EXTERNAL_DECLARATION) {
-                        ptnode.insertElementBefore(child, element0);
-                        ptnode.insertElementBefore(child, element1);
-                        System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
-                        ptnode.remove(child);
-                        i--;
-                        break;
+                    break;
+                case TRANSLATION_UNIT:
+                    // childs = ["translation_unit","external_declaration"]
+                    if (ptnodeChild.getElements().size() == 2) {
+                        PTElement element0 = ptnodeChild.getElements().get(0);
+                        PTElement element1 = ptnodeChild.getElements().get(1);
+                        if (element0 instanceof PTNode && ((PTNode)element0).getNonterminal() == Nonterminals.TRANSLATION_UNIT && element1 instanceof PTNode && ((PTNode)element1).getNonterminal() == Nonterminals.EXTERNAL_DECLARATION) {
+                            ptnode.insertElementBefore(child, element0);
+                            ptnode.insertElementBefore(child, element1);
+                            System.out.println("(recursive) removed " + ptnodeChild.getNonterminal());
+                            ptnode.remove(child);
+                            i--;
+                            break;
+                        }
                     }
-                }
-                break;
+                    break;
             }
         }
     }
