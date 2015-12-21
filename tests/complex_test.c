d0f3e558cc75960c08b9103d7f885c3160b2ddd5
dmitrii64 <dmitriizykov64@gmail.com>
2015-12-21 17:13:00 +0300

    RE IM Test file

--- tests/complex_test.c
+++ tests/complex_test.c
@@ -2,11 +2,15 @@
 int main()
 {
 	complex d = 33 + 22i;
-    complex e = 0 + 22i;
-    complex f = 111i;
-    complex y = 34;
+	complex k = 0.1+123.1i;
     int a;
+    float b = re(d);
     d = -1 - 2i;
+    a = re(d);
+    a = im(d);
+    a = re(22+1i);
     a = 5;
+    b = im(k);
+
 	return 0;
 }
