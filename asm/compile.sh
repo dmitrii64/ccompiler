7856ca538a1c7d3ec97e3e890b42ec1893a631a7
dmitrii64 <dmitriizykov64@gmail.com>
2015-12-21 17:53:15 +0300

    x64 assembler example

--- asm/compile.sh
+++ asm/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nasm input.asm -f aout -o output.o
-ld -m elf_i386 -s -o output output.o
+nasm input.asm -f elf64 -o output.o
+ld -m elf_x86_64 -s -o output output.o
--- asm/input.asm
+++ asm/input.asm
@@ -1,6 +1,69 @@
 global _start
 
+section .data
+msg:	db	"Hello, world!", 10
+.len:	equ	$ - msg
+nl:		db	10
+
+section .bss
+    numbuf resb 10
+
 section .text
+itoa:
+	enter 4,0
+	lea r8,[numbuf+10]
+	mov rcx,10
+	mov [rbp-4],dword 0
+
+	.divloop:
+	xor rdx,rdx
+	idiv rcx
+	add rdx,0x30
+	dec r8
+	mov byte [r8],dl
+	inc dword [rbp-4]
+
+	cmp rax,0
+	jnz .divloop
+
+	mov rax,r8
+	mov rcx,[rbp-4]
+
+	leave
+	ret
+
+clean_buf:
+	lea r8,[numbuf+10]
+	mov rcx,10
+	.clear_loop:
+	mov byte[r8],0
+	dec r8
+	dec rcx
+	cmp rcx,0
+	jnz .clear_loop
+	xor rcx,rcx
+	xor r8,r8
+	ret
+
+print_num:
+	call clean_buf
+	call itoa
+	mov eax, 4
+    mov ebx, 1
+    mov ecx, numbuf
+    mov edx, 10
+    int 0x80
+    ret
+
+println:
+	mov eax, 4
+    mov ebx, 1
+    mov ecx, nl
+    mov edx, 1
+    int 0x80
+    ret
+
+
 _start:
 	mov	eax, 4 ; write
 	mov	ebx, 1 ; stdout
@@ -8,10 +71,15 @@ _start:
 	mov	edx, msg.len
 	int	0x80   ; write(stdout, msg, strlen(msg));
 
+	mov rax, 1234
+	call print_num
+	call println
+
+	mov rax, 666
+    call print_num
+    call println
+
+
 	mov	eax, 1 ; exit
 	mov	ebx, 0
 	int	0x80   ; exit(0)
-
-section .data
-msg:	db	"Hello, world!", 10
-.len:	equ	$ - msg
